c41476b2e8ab9c7ee54d36af5c131983be94e636
fathema15 <fathema.tuz.zohura@g.bracu.ac.bd>
2025-08-21 20:40:04 +0600

    docmodel usrmodel backend done

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Admin Login Controller
+export const authAdmin = async (req, res) => {
+    try{
+       const {atoken} = req.headers 
+       if(!atoken) {return res.json({success:false,message:"Not Authorized Login again"})} 
+       const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
+       if (token_decode != process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+        return res.json({success:false,message:"Not Authorized login again"})
+       }
+       next()
+    }catch (error) {
+        console.log(error)
+    res.json({success:false,message:error.message})
+    }
+
+}
+    
+export default authAdmin 
